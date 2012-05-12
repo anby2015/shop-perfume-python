9c1b967bc9b2b529b91e1ed2639a3c427f7d3ab5
Thinh.onBlurvn <Thinh.onBlurvn@gmail.com>
2012-05-12 15:51:24 +0000

    add tagging function

--- PerfumeShop/urls.py
+++ PerfumeShop/urls.py
@@ -13,5 +13,8 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^search/result/$', views.search_result, name='search_result'),
     url(r'^search/result/(?P<pIndex>[\d]+)/(?P<pSize>[\d]+)/(?P<orderBy>[-\w]+)/(?P<sortOrder>[-\w]+)/(?P<mode>[-\w]+)/$', views.search_result, name='search_result_detail'),
+    url(r'^tags/$', views.tags),
+    url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/$',views.with_tag, name='tag_list'),
+    url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/(?P<pIndex>[\d]+)/(?P<pSize>[\d]+)/(?P<orderBy>[-\w]+)/(?P<sortOrder>[-\w]+)/(?P<mode>[-\w]+)/$', views.with_tag, name='tag_list_detail'),
     url('', views.index, name='index'),
 )
